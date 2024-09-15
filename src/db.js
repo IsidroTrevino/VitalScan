868d5639af788df1ac39aebbb969e240e062936a
IsidroTrevino <A00837104@tec.mx>
2024-09-14 18:49:42 -0600

    added environment variables

--- src/db.js
+++ src/db.js
@@ -1,9 +1,20 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+// Log environment variables to debug
+console.log('HOST:', process.env.HOST);
+console.log('USER:', process.env.USER);
+console.log('PASSWORD:', process.env.PASSWORD);
+console.log('PORT:', process.env.PORT);
+console.log('DATABASE:', process.env.DATABASE);
+
+// Create a connection pool using environment variables
 export const pool = mysql.createPool({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    port: process.env.port,
-    database: process.env.database
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+    database: process.env.DATABASE
 });
